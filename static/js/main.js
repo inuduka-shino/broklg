85d95e5f6f7be62371f291ef9199f3ff4aab69e5
n.m.mobile <hihinuma@gmail.com>
2017-10-27 07:45:09 +0900

    quagga start

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,32 @@
 /*eslint-env browser */
 /*eslint no-console: off */
 /*global define */
+/*member */
 
-define(() => {
+
+define((require) => {
+  const quagga = require('quagga'),
+        {
+          $
+        } = require('./domUtil');
+
+  const $barcode = $('barcode');
   //
+  quagga.init({
+    inputStream : {
+      name : 'Live',
+      type : 'LiveStream',
+      target: $barcode.dom
+    },
+    decoder : {
+      readers : ['code_128_reader']
+    }
+  }, (err) => {
+      if (err) {
+          console.log(err);
+          return;
+      }
+      console.log('Initialization finished. Ready to start');
+      quagga.start();
+  });
 });
--- static/js/requirejs-config.js
+++ static/js/requirejs-config.js
@@ -8,4 +8,14 @@ require.config({
     'maquette': ['/lib/maquette/maquette.min',],
     'quagga': ['../lib/quagga/quagga.min',],
   },
+  shim: {
+    'quagga': {
+          exports: 'quagga',
+          init() {
+            //eslint-disable-next-line no-undef
+            this.quagga = Quagga;
+          }
+    }
+  }
+
 });
