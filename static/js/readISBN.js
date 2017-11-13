cf5cc7744d986122502fffcb396ae651e24c9570
n.m.mobile <hihinuma@gmail.com>
2017-11-14 08:04:01 +0900

    quqgga start/stop no use

--- static/js/readISBN.js
+++ static/js/readISBN.js
@@ -6,8 +6,48 @@ define((require) => {
   const quagga = require('quagga'),
         isbnjs = require('isbnjs');
 
-  function detectISBNStart(fb) {
+  function initQuagga(cntxt, resolve, reject) {
+    quagga.init({
+      inputStream : {
+        name : 'Live',
+        type : 'LiveStream',
+        target: cntxt.dom,
+        constraints: {
+          //width: 640,
+          //height: 300,
+          facingMode: 'environment',
+        },
+      },
+      decoder : {
+        readers : ['ean_reader'],
+
+        debug: {
+            drawBoundingBox: true,
+            showFrequency: true,
+            drawScanline: true,
+            showPattern: true,
+        },
+      },
+      locator: {
+        //halfSample: true,
+        patchSize: 'x-large', // x-small, small, medium, large, x-large
+      },
+    }, (err) => {
+        if (err) {
+            reject(err);
+            return;
+        }
+        // quagga.stop();
+        resolve();
+    });
+  }
+  function detectISBNStart(cntxt) {
+    // quagga.stop();
     quagga.onDetected((data) => {
+      const cb = cntxt.detectISBNcb;
+      if (cb === null) {
+        return;
+      }
       const retCode = data.codeResult.code;
       //msg(`-- onDetect:${retCode}`);
       const isbncode = isbnjs.parse(retCode);
@@ -15,42 +55,38 @@ define((require) => {
           return;
       }
       if (isbncode.isIsbn13()) {
-          fb(isbncode.asIsbn13(true));
+          cb(isbncode.asIsbn13(true));
           return;
       }
       if (isbncode.isIsbn10()) {
-          fb(isbncode.asIsbn13(true));
+          cb(isbncode.asIsbn13(true));
           return; //eslint-disable-line no-useless-return
       }
+      return; //eslint-disable-line no-useless-return
     });
   }
 
   function abort(cntxt) {
-
-    return cntxt.initialed.then(()=>{
-      // quagga.stop();
-      cntxt.hide();
-
-      if (cntxt.detecteAbort !== null) {
-        cntxt.detecteAbort();
-      }
-      cntxt.detecteAbort = null;
-    });
+    //quagga.stop();
+    cntxt.hide();
+    if (cntxt.detecteAbort !== null) {
+      cntxt.detecteAbort();
+    }
+    cntxt.detectISBNcb = null;
+    cntxt.detecteAbort = null;
   }
 
-  async function start(cntxt) {
-    await cntxt.initialed;
+  function start(cntxt) {
     cntxt.show();
-    quagga.show();
-
-    const prmsDetectISBN = new Promise((resolve) =>{
-      detectISBNStart((isbn)=>{
-          resolve(isbn);
-      });
+    //quagga.start();
+    const prms = new Promise((resolve,reject) =>{
+      cntxt.detectISBNcb = resolve;
+      cntxt.detecteAbort = reject;
     });
-    const isbn = await prmsDetectISBN;
-    abort(cntxt);
-    return isbn;
+    cntxt.detectISBNcb = null;
+    cntxt.detecteAbort = null;
+    prms.then(abort.bind(null, cntxt));
+    return prms;
   }
 
   function generate({
@@ -63,45 +99,13 @@ define((require) => {
       dom, // barcode detection camera video
       show, // dom show function
       hide, // dom hide function
+      detectISBNcb: null,
       detecteAbort: null,
       message,
     };
 
-    cntxt.initialed = new Promise((resolve, reject) => {
-      quagga.init({
-        inputStream : {
-          name : 'Live',
-          type : 'LiveStream',
-          target: cntxt.dom,
-          constraints: {
-            //width: 640,
-            //height: 300,
-            facingMode: 'environment',
-          },
-        },
-        decoder : {
-          readers : ['ean_reader'],
-
-          debug: {
-              drawBoundingBox: true,
-              showFrequency: true,
-              drawScanline: true,
-              showPattern: true,
-          },
-        },
-        locator: {
-          //halfSample: true,
-          patchSize: 'x-large', // x-small, small, medium, large, x-large
-        },
-      }, (err) => {
-          if (err) {
-              reject(err);
-              return;
-          }
-          // quagga.stop();
-          resolve();
-      });
-    });
+    cntxt.initialed = new Promise(initQuagga.bind(null, cntxt));
+    cntxt.initialed.then(detectISBNStart.bind(null,cntxt));
     return {
         start: start.bind(null, cntxt),
         abort: abort.bind(null, cntxt),
