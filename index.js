4380553b93f4b9b74f9fe3afa777e0ba0fd1829f
nknshm <hihinuma@gmail.com>
2017-12-10 15:53:43 +0900

    start index.html

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ router.get('/lib/isbnjs/*', serv({
 router.get('', serv({
   rootDir: path.join(__dirname, 'static/'),
   rootPath: '/',
-  index: 'test.html',
+  index: 'index.html',
 }));
 router.get(/.*/, serv({
   rootDir: path.join(__dirname, 'static/'),
